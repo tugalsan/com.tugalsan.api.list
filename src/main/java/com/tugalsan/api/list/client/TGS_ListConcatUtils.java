20abbbfaefd773beae132d72338c21873780669c
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-08-29 07:56:32 +0300

    Update TGS_ListConcatUtils.java

--- src/main/java/com/tugalsan/api/list/client/TGS_ListConcatUtils.java
+++ src/main/java/com/tugalsan/api/list/client/TGS_ListConcatUtils.java
@@ -1,23 +1,29 @@
 package com.tugalsan.api.list.client;
 
 import com.tugalsan.api.function.client.TGS_Func_OutBool_In2;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class TGS_ListConcatUtils {
 
-    public static List<String> distinctString(boolean ordered, List<String>... lists) {
-        return filtered((union, item) -> !union.contains(item), ordered, lists);
-    }
-
-    public static List<Long> distinctLong(boolean ordered, List<Long>... lists) {
+//    public static List<String> distinctString(boolean ordered, List<String>... lists) {
+//        return filtered((union, item) -> !union.contains(item), ordered, lists);
+//    }
+//
+//    public static List<Long> distinctLong(boolean ordered, List<Long>... lists) {
+//        return filtered((union, item) -> {
+//            return union.stream().filter(it -> it != item.longValue()).findAny().isPresent();//GWT does not like isEmpty; check on 2.10 version again!
+//        }, ordered, lists);
+//    }
+//
+//    public static List<Integer> distinctInteger(boolean ordered, List<Integer>... lists) {
+//        return filtered((union, item) -> {
+//            return union.stream().filter(it -> it != item.longValue()).findAny().isPresent();//GWT does not like isEmpty; check on 2.10 version again!
+//        }, ordered, lists);
+//    }
+    public static <T> List<T> distinct(boolean ordered, List<T>... lists) {
         return filtered((union, item) -> {
-            return union.stream().filter(it -> it != item.longValue()).findAny().isPresent();//GWT does not like isEmpty; check on 2.10 version again!
-        }, ordered, lists);
-    }
-
-    public static List<Integer> distinctInteger(boolean ordered, List<Integer>... lists) {
-        return filtered((union, item) -> {
-            return union.stream().filter(it -> it != item.longValue()).findAny().isPresent();//GWT does not like isEmpty; check on 2.10 version again!
+            return !union.contains(item);
         }, ordered, lists);
     }
 
@@ -38,24 +44,32 @@ public class TGS_ListConcatUtils {
         return union;
     }
 
-    public static String[] concat(String[] s1, String[] s2) {
-        var o = new String[s1.length + s2.length];
+    public static <T> T[] concat(Class<T> clazz, T[] s1, T[] s2) {
+        @SuppressWarnings("unchecked")
+        T[] o = (T[]) Array.newInstance(clazz, s1.length + s2.length);
         System.arraycopy(s1, 0, o, 0, s1.length);
         System.arraycopy(s2, 0, o, s1.length, s2.length);
         return o;
     }
 
-    public static Object[] concat(Object[] s1, Object[] s2) {
-        var o = new Object[s1.length + s2.length];
-        System.arraycopy(s1, 0, o, 0, s1.length);
-        System.arraycopy(s2, 0, o, s1.length, s2.length);
-        return o;
-    }
-
-    public static Object[][] concat(Object[][] s1, Object[][] s2) {
-        var o = new Object[s1.length + s2.length][];
-        System.arraycopy(s1, 0, o, 0, s1.length);
-        System.arraycopy(s2, 0, o, s1.length, s2.length);
-        return o;
-    }
+//    public static String[] concat(String[] s1, String[] s2) {
+//        var o = new String[s1.length + s2.length];
+//        System.arraycopy(s1, 0, o, 0, s1.length);
+//        System.arraycopy(s2, 0, o, s1.length, s2.length);
+//        return o;
+//    }
+//
+//    public static Object[] concat(Object[] s1, Object[] s2) {
+//        var o = new Object[s1.length + s2.length];
+//        System.arraycopy(s1, 0, o, 0, s1.length);
+//        System.arraycopy(s2, 0, o, s1.length, s2.length);
+//        return o;
+//    }
+//
+//    public static Object[][] concat(Object[][] s1, Object[][] s2) {
+//        var o = new Object[s1.length + s2.length][];
+//        System.arraycopy(s1, 0, o, 0, s1.length);
+//        System.arraycopy(s2, 0, o, s1.length, s2.length);
+//        return o;
+//    }
 }
